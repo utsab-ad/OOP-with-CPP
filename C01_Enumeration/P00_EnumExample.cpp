7c5d5a84cc8c68f7c49745f7742894646c12bb16
utsab-ad <utasbadhikari075@gmail.com>
2025-05-16 09:00:35 -0600

    updated +1

---
+++ C01_Enumeration/P00_EnumExample.cpp
@@ -0,0 +1,27 @@
+// Example Program to define "enum" with user specific integer DataType 
+
+#include<iostream>
+    using namespace std;
+      enum Days {
+            sunday,
+            monday,
+            tuesday,
+            wednesday,
+            thursday = 10,
+            friday,
+            saturday
+        };
+    int main() {
+
+      Days today, tom;
+
+      today = friday;
+      tom = saturday;
+      cout<<"The enum value of Today is "<<today<<endl;
+      cout<<"The enum value of Tomorrow is "<<tom<<endl;
+
+        return 0;
+
+
+    }
+
