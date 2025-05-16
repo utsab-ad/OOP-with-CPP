a2f2793a979fb20039f27e78424bada04bfc721f
utsab-ad <utasbadhikari075@gmail.com>
2025-05-15 21:36:35 -0600

    chapter-1 added

---
+++ C01_Enumeration/P01_EnumTraffic.cpp
@@ -0,0 +1,29 @@
+// Example Program to define "enum" "Traffic_Light" and  displaying the enum values
+
+#include<iostream>
+
+    using namespace std;
+
+        enum Traffic_Light {
+        red=5,
+        green,
+        yell,
+        };
+
+
+    int main() {
+
+        Traffic_Light go, wait, stop;
+
+        stop=red;
+        wait=yell;
+        go=green;
+
+        cout<<"The respective traffic signals using enum are : "<<endl;
+        cout<<"GO: "<<go<<endl;
+        cout<<"Wait: "<<wait<<endl;
+
+        cout<<"Stop: "<<stop<<endl;
+        return 0;
+
+    }
